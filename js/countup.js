99e83e686b389590e9350d2ddeb10c522375e18d
UmaEra <124608386+NotYarazi@users.noreply.github.com>
2024-08-02 22:34:26 +0100

    quick fix

--- js/countup.js
+++ js/countup.js
@@ -14,6 +14,6 @@ const countdownInterval = setInterval(() => {
     document.getElementById('minutes').innerHTML = String(minutes).padStart(2, '0');
     document.getElementById('seconds').innerHTML = String(seconds).padStart(2, '0');
 
-    document.getElementById('message').textContent = 'making 3.6.1';
+    document.getElementById('message').textContent = 'making version 3.6.1';
     document.getElementById('message').style.opacity = '1';
 }, 500);
