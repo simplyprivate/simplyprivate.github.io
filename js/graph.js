2e3dc52e0e105be3c2944cb69f5a59eab1e14997
UmaEra <yaraziasheut11@gmail.com>
2024-10-02 20:59:42 +0100

    september stats

--- js/graph.js
+++ js/graph.js
@@ -2,11 +2,11 @@ const ctx = document.getElementById('statisticsChart').getContext('2d');
 const statisticsChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['December 2023', 'January 2024', 'February 2024', 'March 2024', 'April 2024', 'May 2024', 'June 2024', 'July 2024', 'August 2024'],
+        labels: ['December 2023', 'January 2024', 'February 2024', 'March 2024', 'April 2024', 'May 2024', 'June 2024', 'July 2024', 'August 2024', 'September 2024'],
         datasets: [
             {
                 label: 'Downloads',
-                data: [18, 97, 355, 439, 1633, 1203, 3062, 4238, 7206],
+                data: [18, 97, 355, 439, 1633, 1203, 3062, 4238, 7206, 12797],
                 borderColor: 'rgba(75, 192, 192, 1)',
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 fill: false,
@@ -14,7 +14,7 @@ const statisticsChart = new Chart(ctx, {
             },
             {
                 label: 'Views',
-                data: [69, 188, 838, 791, 3085, 5923, 5406, 9256, 17801],
+                data: [69, 188, 838, 791, 3085, 5923, 5406, 9256, 17801, 23405],
                 borderColor: 'rgba(153, 102, 255, 1)',
                 backgroundColor: 'rgba(153, 102, 255, 0.2)',
                 fill: false,
